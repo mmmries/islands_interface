2e6b46f9455ef25d0d57f9ca3b297a116db67c9f
Michael Ries <michael@riesd.com>
2018-03-21 10:51:28 -0600

    mix format

--- config/config.exs
+++ config/config.exs
@@ -10,8 +10,7 @@ config :islands_interface, IslandsInterfaceWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "swK8ceCR3v5RynS+ELQ7yHyttiX2i4WYAlX1Ac2yXr0uBxi0hwAW3S/54xSN4PWz",
   render_errors: [view: IslandsInterfaceWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: IslandsInterface.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: IslandsInterface.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -20,4 +19,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
